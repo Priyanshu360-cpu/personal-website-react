4d247ae8a38ec789699bc9303caaa5b26a0e9e0e
Priyanshu <79325092+Priyanshu360-cpu@users.noreply.github.com>
2022-03-19 23:12:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,31 +46,14 @@ function App() {
     
     <>
     <body>
-      
     <div class = "app">
     <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css'></link>
     <video autoPlay muted loop id="myVideo" source src={video} type="video/mp4">
 </video>
-<div class = "blinker"><a href="https://ww.google.com"><Appu></Appu></a></div>
+<div class = "blinker"><a href="https://www.google.com"><Appu></Appu></a></div>
 <Appus></Appus>
-<div class = "beluw">
-<div class = "bright">
-     
-     <script source="./button.js"></script>
-          <div class="contentse">
-             <i class="fas fa-sun"></i>
-             &nbsp;&nbsp;&nbsp;
-             <input type="checkbox" id="tooglenight" class="cbx hidden"/>
-             <label for="tooglenight" class="switch"></label>
-             &nbsp;&nbsp;&nbsp;
-             <i class="fas fa-moon"></i>
-         </div>
-       </div>
-</div>
-<div class = "App">
 
-     
-</div>
+
 <div class = "viewport-header">
 <div class = "content">
   
@@ -84,8 +67,9 @@ function App() {
   <p><img src = "https://cdn.discordapp.com/emojis/779358851690528808.gif?size=32"></img>My hobbies are Playing cricket and reading books . </p>
   <p><img src = "https://cdn.discordapp.com/emojis/779358851690528808.gif?size=32"></img>I have a deep interest in space and everything related with space attracts me towards it. </p>
   <p> <img src = "https://cdn.discordapp.com/emojis/779358851690528808.gif?size=32"></img>am a keen obeserver and i actively write blogs on my Linked In account :) </p></div>
+  
 
-  <a href = "https://www.goolge.com"><img src = "https://media.discordapp.net/attachments/750089917501014036/913134398914834502/IMG_20211023_175855_214.jpg" class = "App-logo" alt = "logo" ></img></a>
+  
 </div>
 </div>
 <div class = "newaa">
@@ -95,35 +79,11 @@ function App() {
             </marquee>
             </div>
             <div class = "texta">
-              <div class = "proj"><img src="https://cdn.discordapp.com/avatars/838697702760906802/09646c544c38aee2dd428999f56a2097.png?size=64"></img><p>Caretaker is a cross platform Mobile App to monitor and maintain your health</p><p>it was built with xamarain and c sharp on vs 2022</p><p>Track your health,find solutions and stay healthy with caretaker.</p><button class="button button2">Visit</button></div>
-              <div class = "proi1"><img id="object1" src="https://cdn.discordapp.com/avatars/741280410180386947/97ddc40395dfa5312cdbf02f4da07d7a.png?size=64"></img><p>Techie is a Multifunctioncal bot built with Javascript</p><p>From Catching Sockets and lavalink to providing safe environemnt It does all</p><p>Easily listen to songs and show your friends what you are listening with the rpc client with Techie.</p><button class="button button2">Visit</button></div>
+              <div class = "proj"><img src="https://cdn.discordapp.com/avatars/838697702760906802/09646c544c38aee2dd428999f56a2097.png?size=64"></img><p>Caretaker is a cross platform Mobile App to monitor and maintain your health</p><p>it was built with xamarain and c sharp on vs 2022</p><button class="button button2">Visit</button></div>
+              <div class = "proi1"><img id="object1" src="https://cdn.discordapp.com/avatars/741280410180386947/97ddc40395dfa5312cdbf02f4da07d7a.png?size=64"></img><p>Techie is a Multifunctioncal bot built with Javascript</p><p>From lavalink to providing safe environemnt It does all</p><p> listen to songs and show others what you are listening with Techie.</p><button class="button button2">Visit</button></div>
               <div class = "proj2"><img src="https://cdn.discordapp.com/avatars/798655819033608262/bb6accbe3c1ac258c138d151f503911a.png?size=64"></img><p>Cordy is a npm package which eases your bot making</p><p>Cordy has support for Discordjs</p><p>You can build a bot with cordy with no coding experience before.</p><button class="button button2">Visit</button></div>
-            <iframe src="https://discordapp.com/widget?id=222078108977594368&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-<div class = "cardo">
-<div class="skillcard">
-<h1>My Skills</h1>
+            <iframe src="" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
 
-<p>JAVASCRIPT</p>
-<div class="container">
-  <div class="skills html"><span>90%</span></div>
-</div>
-
-<p >GOLANG</p>
-<div class="container">
-  <div class="skills css"><span>80%</span></div>
-</div>
-
-<p>C FAMILY</p>
-<div class="container">
-  <div class="skills js"><span>65%</span></div>
-</div>
-
-<p>JAVA</p>
-<div class="container">
-  <div class="skills php"><span>60%</span></div>
-</div>
-</div>
-</div>
 </div> 
 
 <ul>
@@ -131,7 +91,7 @@ function App() {
   <li><a class="active" href="#home">Priyanshu</a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
-  <li style={{float:"right"}}><a class="active" href="https://discord.gg/9cKGtRNDqZ"><img src ="https://cdn.discordapp.com/emojis/909511958053658634.gif?size=32"></img></a></li>
+  
   <li style={{float:"right"}}><a class="active" href="https://www.instagram.com/priyanshu.__0007/"><img src ="https://cdn.discordapp.com/emojis/797052330054516747.gif?size=32"></img></a></li>
   <li style={{float:"right"}}><a class="active" href="https://github.com/Priyanshu360-cpu"><img src ="https://cdn.discordapp.com/emojis/880422918268518450.png?size=32"></img></a></li>
 </ul>
@@ -139,14 +99,14 @@ function App() {
 
 
      </div>
-     <div class ="skillsbg"></div>
+     <div class ="skillsbg">
+     
+     </div>
      <div class="conc">
        <p>Made with Reactjs with ❤️</p>
-       <p>Shoot me an email at <a href="tpriyanshu775@gmail.com">tpriyanshu775@gmail.com</a></p>
+       <p>Shoot me an email at <a href="https://www.tpriyanshu775@gmail.com">tpriyanshu775@gmail.com</a></p>
        </div>
-     <div class = "linked"><button onclick={() =><clicker></clicker>} class="button button2">
-     <img src="https://cdn.discordapp.com/emojis/868562770277318698.gif?size=32" /> LinkedIn 
-  </button></div>
+     
      </body>
      
 </>
